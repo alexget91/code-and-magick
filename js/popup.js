da144fb1cdd4e8c2fd8f05d75c5841a18ea32e71
Alex <alex.get91@gmail.com>
2017-09-12 20:34:58 +0300

    Загрузка аватара, инвентарь похожих волшебников

---
+++ js/popup.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Управление всплывающим окном
+(function () {
+
+  var OFFSET = 10; // px
+
+  var popupElement = document.createElement('div');
+  popupElement.classList.add('popup');
+  popupElement.style.display = 'none';
+  document.body.appendChild(popupElement);
+
+  var onMouseMove = function (evt) {
+    popupElement.style.top = evt.pageY + OFFSET + 'px';
+    popupElement.style.left = evt.pageX + OFFSET + 'px';
+  };
+
+  window.popup = function (target, callback) {
+    var onMouseOut = function () {
+      popupElement.style.display = 'none';
+      target.removeEventListener('mousemove', onMouseMove);
+      target.removeEventListener('mouseleave', onMouseOut);
+    };
+
+    target.addEventListener('mouseenter', function () {
+      popupElement.innerHTML = callback();
+      popupElement.style.display = 'block';
+
+      target.addEventListener('mousemove', onMouseMove);
+      target.addEventListener('mouseleave', onMouseOut);
+    });
+  };
+
+})();
